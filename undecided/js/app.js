ec65f1c99699206aed3eba76ea98fc3dbf19ff2b
Victoria Thomas <emailvictoriathomas@gmail.com>
2024-02-08 15:47:19 -0600

    Adds first and second viewer screens.

---
+++ undecided/js/app.js
@@ -0,0 +1,15 @@
+/* 
+create variables
+game board
+create the snake
+make the snake move in one direction
+change directions
+make food
+make food randomly generate
+what happens when snake touches food 
+what happens after (snake grows) (points)
+save scores
+how to end games
+reset or start game
+
+*/
