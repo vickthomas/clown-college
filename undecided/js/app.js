d9f4956b641931e770de4f2a90d41f0aa26b344d
Victoria Thomas <emailvictoriathomas@gmail.com>
2024-02-12 20:56:28 -0600

    Adds psuedocode.

--- undecided/js/app.js
+++ undecided/js/app.js
@@ -12,4 +12,41 @@ save scores
 how to end games
 reset or start game
 
+
+Decisions
+In this project, knowledge of how decisions should be utilized in code will be illustrated with proper use of: 
+If, then statements
+If, else statements
+Switch
+DOM Manipulation
+In this project, DOM Manipulation will be utilized in multiple ways. Proper use of this structure will show the coder understands the following: (resource)
+What it is
+How to select elements
+getElementById()
+getElementByClassName()
+getElementByTagName()
+How to select elements with CSS Selectors
+querySelector()
+
 */
+let focus = 50;
+let connections = 20;
+
+// Outcome: {
+//     
+//     label: 'No Way',
+//     message: 'You\'ll incur much less debt and start earning earlier, but you might find yourself under-qualified for many positions.',
+//     
+//     OutcomeValues: [{
+//         Value: {
+//             label: 'Connections',
+//             amount: -100
+//         }
+//     }]
+// }
+// }
+
+// Pre-College
+
+let button1 = document.querySelector("#button1");
+let button2 = document.quesrySelector("#button2");
